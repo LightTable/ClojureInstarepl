d50624999426fdf061ab004ae9963da9846cc55f
Gabriel Horner <gabriel.horner@gmail.com>
2015-12-31 17:24:15 -0500

    Bump to 0.3.1

---
+++ lein-light-nrepl-instarepl/CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.3.1
+* Use latest lein-light-nrepl that has instarepl removed
+
+## 0.3.0
+* Initial release!
--- lein-light-nrepl-instarepl/project.clj
+++ lein-light-nrepl-instarepl/project.clj
@@ -1,8 +1,8 @@
-(defproject lein-light-nrepl-instarepl "0.3.0"
+(defproject lein-light-nrepl-instarepl "0.3.1"
   :description "nrepl client for Clojure instarepl"
   :url "https://github.com/LightTable/ClojureInstarepl/tree/master/lein-light-nrepl-instarepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [lein-light-nrepl "0.2.0"]]
+                 [lein-light-nrepl "0.3.0"]]
   :jvm-opts ["-Xmx1g"])
